964684581462ddd5382feffccc942f7a34a1dfee
enaut aa <enaut.irakasle@gmail.com>
2024-03-09 00:29:56 +0000

    probatzearren

--- routes/web.php
+++ routes/web.php
@@ -22,5 +22,6 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('clientes', App\Http\Controllers\ClienteController::class);
+
 Route::resource('pedidos', App\Http\Controllers\PedidoController::class);
 
